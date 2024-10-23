3d43cac0f1d58b6b641c2639346afaee73c0892d
YashPatel50 <patelshay8@gmail.com>
2024-10-23 19:43:10 -0400

    Created a basic Responsive Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorPalette="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav""main"`,
+        lg: `"nav nav""aside main"`, //Bigger than 1024 pixels
+      }}
+    >
+      <GridItem area="nav" bg="blue.200">
+        Nav
+      </GridItem>
+      <GridItem area="aside" bg="gray.400" hideBelow="lg">
+        Aside
+      </GridItem>
+      <GridItem area="main" bg="purple.400">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
